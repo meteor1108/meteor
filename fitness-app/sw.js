6c3673c00aac1786a8e0d6994c54ee6ebba08394
meteor1108 <324340178+meteor1108@users.noreply.github.com>
2026-09-08 18:43:30 +0800

    Add curated foods and supplement reminders

--- fitness-app/sw.js
+++ fitness-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitness-v7';
+const CACHE = 'fitness-v8';
 const ASSETS = ['./', './index.html', './bg.jpg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
