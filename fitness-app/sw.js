0106a0ac0c75a1ee5583746ae51b92682c6067fb
meteor1108 <324340178+meteor1108@users.noreply.github.com>
2026-09-16 00:13:41 +0800

    Animate daily check-in feedback

--- fitness-app/sw.js
+++ fitness-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitness-v13';
+const CACHE = 'fitness-v14';
 const ASSETS = ['./', './index.html', './bg.jpg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
