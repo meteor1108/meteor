88131cdba0a3ce871abf6fbcc50389a7a4d8643c
meteor1108 <125457649@qq.com>
2026-09-18 11:55:10 +0800

    Compact workout UI and refresh pull day

--- fitness-app/sw.js
+++ fitness-app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fitness-v17';
+const CACHE = 'fitness-v18';
 const ASSETS = ['./', './index.html', './bg.jpg', './icon-192.png', './icon-512.png', './manifest.json'];
 
 self.addEventListener('install', e => {
